3baeba3ecf82a74d0cfc0c706eafdfb8821c6105
Kostya <kostia-kovallyk@yandex.ru>
2024-01-31 12:17:45 +0500

    Add viewset for comment and review, add new permission class

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -9,3 +9,18 @@ class AdminAnonPermission(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
                 and (request.user.role == User.ADMIN or request.user.is_staff))
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,5 @@
+from django.forms import ValidationError
+from django.shortcuts import get_object_or_404
 from django.db.models import Avg
 from rest_framework import status, viewsets
 from rest_framework.response import Response
@@ -5,11 +7,12 @@ from rest_framework.views import APIView
 
 
 from api.filters import TitleFilter
-from api.permission import AdminAnonPermission
-from api.serializers import (CategorySerializer, GenreSerializer,
-                             ReadTitleSerializer, WriteTitleSerializer)
+from api.permission import AdminAnonPermission, IsOwnerOrReadOnly
+from api.serializers import (CategorySerializer, CommentSerializers,
+                             GenreSerializer, ReadTitleSerializer,
+                             ReviewSerializer, WriteTitleSerializer)
 from api.viewsets import ListCreateDelViewSet
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, Review
 
 
 class APISignup(APIView):
@@ -51,3 +54,41 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 class UsersViewSet(viewsets.ModelViewSet):
     pass
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    model = CommentSerializers
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def get_post(self):
+        return get_object_or_404(Review, pk=self.kwargs['review_id'])
+
+    def perform_create(self, serializer):
+        post = self.get_post()
+        serializer.save(author=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = self.get_post()
+        return post.comments.all()
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs['title_id']
+        title = get_object_or_404(Title, pk=title_id)
+        author = self.request.user
+        existing_review = Review.objects.filter(
+            title=title, author=author
+        ).first()
+        if existing_review:
+            raise ValidationError('Вы уже оставляли отзыв этому произведению')
+        serializer.save(title=title, author=author)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
