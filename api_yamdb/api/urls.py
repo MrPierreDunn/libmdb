034d3318da614482462cda3d2f842ea5e491ec92
Kostya <kostia-kovallyk@yandex.ru>
2024-02-01 20:31:14 +0500

    Add endpoints for comments and review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,29 +1,39 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import (CategoryViewSet, GenreViewSet,
-                       TitleViewSet)
+from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                       ReviewViewSet, TitleViewSet)
 
 app_name = 'api'
 
-router = routers.DefaultRouter()
-router.register(
+v1_router = routers.DefaultRouter()
+v1_router.register(
     'categories',
     CategoryViewSet,
     basename='categories'
 )
-router.register(
+v1_router.register(
     'genres',
     GenreViewSet,
     basename='genres'
 )
-router.register(
+v1_router.register(
     'titles',
     TitleViewSet,
     basename='titles'
 )
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
