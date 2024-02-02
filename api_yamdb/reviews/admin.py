1dae224ebed0799be5eb8f4bd527386fe3480d99
Alexandr V. Kostin <kavnsk@gmail.com>
2024-02-03 00:29:06 +0700

    modified:   api_yamdb/api_yamdb/settings.py 	new file:   api_yamdb/reviews/__init__.py 	new file:   api_yamdb/reviews/admin.py 	new file:   api_yamdb/reviews/management/__init__.py 	new file:   api_yamdb/reviews/management/commands/__init__.py 	modified:   api_yamdb/reviews/management/commands/fill_db_from_csv.py 	new file:   api_yamdb/reviews/migrations/0001_initial.py 	new file:   api_yamdb/reviews/migrations/__init__.py 	modified:   requirements.txt

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+
+from reviews.models import Category, Comment, Genre, Review, Title
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'year',
+        'category',
+    )
+    list_filter = ('category',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'author',
+        'score',
+        'pub_date',
+    )
+    list_filter = ('author',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'review',
+        'author',
+        'pub_date',
+    )
+    list_filter = ('author',)
+    empty_value_display = '-пусто-'
